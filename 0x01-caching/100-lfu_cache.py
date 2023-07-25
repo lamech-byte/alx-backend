cc4fbd1004aebf963dcdf05fefad935588f8863c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-25 18:33:58 +0300

    100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -4,7 +4,6 @@
 """
 
 from base_caching import BaseCaching
-from collections import defaultdict
 
 
 class LFUCache(BaseCaching):
@@ -12,36 +11,48 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """ Initialize """
         super().__init__()
-        self.frequency = defaultdict(int)
+        self.frequency = {}  # Dictionary to store the frequency of each key
+        self.frequency_of_frequency = {}  # Dictionary to store
         self.min_frequency = 0
-        self.order = []
 
     def update_frequency(self, key):
         """ Update frequency of the key """
-        self.frequency[key] += 1
-        if self.frequency[key] > self.min_frequency:
-            self.min_frequency = self.frequency[key]
+        if key in self.frequency:
+            self.frequency[key] += 1
+        else:
+            self.frequency[key] = 1
+
+        frequency = self.frequency[key]
+        if frequency - 1 in self.frequency_of_frequency:
+            self.frequency_of_frequency[frequency - 1].remove(key)
+
+        if frequency not in self.frequency_of_frequency:
+            self.frequency_of_frequency[frequency] = []
+
+        self.frequency_of_frequency[frequency].append(key)
+
+        if not self.frequency_of_frequency[self.min_frequency]:
+            self.min_frequency += 1
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                lfu_keys = [
-                    k for k, v in self.frequency.items()
-                    if v == self.min_frequency
-                ]
-                for k in self.order[:]:
-                    if k in lfu_keys:
-                        self.order.remove(k)
-                        del self.cache_data[k]
-                        del self.frequency[k]
-                        print("DISCARD: {}".format(k))
+                while not self.frequency_of_frequency[self.min_frequency]:
+                    self.min_frequency += 1
 
-                self.min_frequency += 1
+                lfu_keys = self.frequency_of_frequency[self.min_frequency]
+
+                if len(lfu_keys) == 1:
+                    discarded_key = lfu_keys[0]
+                else:
+                    discarded_key = lfu_keys.pop(0)
+
+                del self.cache_data[discarded_key]
+                self.frequency.pop(discarded_key)
 
             self.cache_data[key] = item
             self.update_frequency(key)
-            self.order.append(key)
 
     def get(self, key):
         """ Get an item by key """
