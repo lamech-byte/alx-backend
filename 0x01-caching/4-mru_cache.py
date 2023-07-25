8f0e666af84dc80e9fc482e2fee44bc1ac8f108e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-25 11:52:47 +0300

    4-mru_cache.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+4-mru_cache.py
+"""
+
+from base_caching import BaseCaching
+
+class MRUCache(BaseCaching):
+    """ MRUCache class that inherits from BaseCaching """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key = self.order.pop()
+                del self.cache_data[mru_key]
+                print("DISCARD: {}".format(mru_key))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
