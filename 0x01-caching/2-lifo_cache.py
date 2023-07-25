8ed66dcd63cc20f8b6be53bbdceb24fa262e4bae
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-25 11:49:16 +0300

    2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+2-lifo_cache.py
+"""
+
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache class that inherits from BaseCaching """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = self.order.pop()
+                del self.cache_data[last_key]
+                print("DISCARD: {}".format(last_key))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
