2b530b19165dbfa17904b35f4d22b0c7bcec6aa5
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-25 11:44:37 +0300

    1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+1-fifo_cache.py
+"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class that inherits from BaseCaching """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                first_key = self.order[0]
+                self.order = self.order[1:]
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
+
