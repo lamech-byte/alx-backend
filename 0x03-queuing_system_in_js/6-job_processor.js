b1cabcca568d2e317869c30cd8af949578ec2204
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-30 20:30:17 +0300

    6-job_processor.js

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,17 @@
+// Import the required modules
+import kue from 'kue';
+
+// Create a Kue queue
+const queue = kue.createQueue();
+
+// Function to send a notification
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+// Process jobs from the push_notification_code queue
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
