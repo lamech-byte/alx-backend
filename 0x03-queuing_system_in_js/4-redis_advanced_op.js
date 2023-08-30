77c21bd71c4050201276ae21f71b2b4ff6ee67dd
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-30 20:11:11 +0300

    4-redis_advanced_op.js

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,36 @@
+// Import the required modules
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Handle successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Handle connection error
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+// Create Hash using hset
+const createHash = () => {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+};
+
+// Display Hash using hgetall
+const displayHash = () => {
+  client.hgetall('HolbertonSchools', (error, reply) => {
+    console.log(reply);
+  });
+};
+
+// Call the functions
+createHash();
+displayHash();
