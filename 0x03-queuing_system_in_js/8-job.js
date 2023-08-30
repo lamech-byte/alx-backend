282c214415cdd613d24fea5a660188810e601abe
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-30 20:48:47 +0300

    8-job.js

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,32 @@
+import kue from 'kue';
+
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData);
+
+    job
+      .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on('failed', (err) => {
+        console.log(`Notification job ${job.id} failed: ${err}`);
+      })
+      .on('progress', (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+
+    job.save((err) => {
+      if (err) {
+        console.error(`Failed to create notification job: ${err}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+  });
+}
+
+export default createPushNotificationsJobs;
