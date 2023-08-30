64d53f3fcce38a4d56ff21b80a0f322925e79b69
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-30 20:42:36 +0300

    7-job_creator.js

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -5,27 +5,48 @@ import kue from 'kue';
 const jobs = [
   {
     phoneNumber: '4153518780',
-    message: 'This is the code 1234 to verify your account'
+    message: 'This is the code 1234 to verify your account',
   },
-  // ... (add more objects here)
+  // ... Add more job objects as needed
 ];
 
 // Create a Kue queue
 const queue = kue.createQueue();
 
-// Loop through the jobs array and create jobs
-jobs.forEach((jobData, index) => {
-  const job = queue
-    .create('push_notification_code_2', jobData)
-    .save((err) => {
-      if (!err) {
-        console.log(`Notification job created: ${job.id}`);
-      }
-    });
+// Function to send a notification
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
 
-  job
-    .on('complete', () => {
-      console.log(`Notification job ${job.id} completed`);
-    })
-    .on('failed', (err) => {
-      console.log(`Notification job ${job.id} failed: ${err}`);
+// Loop through the array of jobs and create a job for each
+jobs.forEach((jobData, index) => {
+  const job = queue.create('push_notification_code_2', jobData);
+
+  // On successful job creation
+  job.save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+  // On job completion
+  job.on('complete', () => {
+    console.log(`Notification job ${job.id} completed`);
+  });
+
+  // On job failure
+  job.on('failed', (errorMessage) => {
+    console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+  });
+
+  // On job progress
+  job.on('progress', (progress, data) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+
+  // Simulate job progress (for demonstration purposes)
+  job.progress(index * 10, 10);
+
+  // Uncomment the line below to simulate job completion (for demonstration purposes)
+  // job.complete();
+});
